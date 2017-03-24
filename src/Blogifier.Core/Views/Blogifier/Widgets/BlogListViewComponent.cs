a6816a3f9fbd86abbcc7901f92131bbaebfd1814
rxtur <rtur.net@gmail.com>
2017-03-23 22:57:27 -0500

    view components

---
+++ src/Blogifier.Core/Views/Blogifier/Widgets/BlogListViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogifier.Core.Views.Blogifier.Widgets
+{
+	[ViewComponent(Name = "Blogifier.Core.Views.Blogifier.Widgets.BlogList")]
+	public class BlogListViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke(int number)
+		{
+			var blogs = new List<string>();
+			blogs.Add("foo");
+			blogs.Add("bar");
+
+			return View(blogs);
+		}
+	}
+}
