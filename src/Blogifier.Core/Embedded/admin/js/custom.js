345cd7cd9c3b6053e21ca20605cee0030e5efbc6
rxtur <rtur.net@gmail.com>
2017-04-08 21:26:19 -0500

    v 0.3

--- src/Blogifier.Core/Embedded/admin/js/custom.js
+++ src/Blogifier.Core/Embedded/admin/js/custom.js
@@ -1,3 +1,11 @@
 $(".app-side-toggle").click(function() {
     $(".app").toggleClass("toggled")
 });
+//
+$(document).ready(function(){
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square',
+    radioClass: 'iradio_square',
+    increaseArea: '10%' // optional
+  });
+});
