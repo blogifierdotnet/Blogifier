d8dfd13b471e6d23c48395a596f762a1bc41c3e2
rxtur <rtur.net@gmail.com>
2017-05-15 19:32:46 -0500

    core themes

--- src/Blogifier.Core/Controllers/BlogController.cs
+++ src/Blogifier.Core/Controllers/BlogController.cs
@@ -1,10 +1,13 @@
-﻿using Blogifier.Core.Data.Interfaces;
+﻿using Blogifier.Core.Common;
+using Blogifier.Core.Data.Interfaces;
 using Blogifier.Core.Data.Models;
 using Blogifier.Core.Services.Syndication.Rss;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Blogifier.Core.Controllers
@@ -28,9 +31,18 @@ namespace Blogifier.Core.Controllers
 
 		public IActionResult Index()
 		{
-			var posts = _db.BlogPosts.All();
-			return View(_theme + "Index.cshtml", posts);
-		}
+            var pager = new Pager(1);
+            var posts = _db.BlogPosts.Find(p => p.Published > DateTime.MinValue, pager);
+            return View(_theme + "Index.cshtml", posts);
+        }
+
+        [Route("{page:int}")]
+        public IActionResult Index(int page)
+        {
+            var pager = new Pager(page);
+            var posts = _db.BlogPosts.Find(p => p.Published > DateTime.MinValue, pager);
+            return View(_theme + "Index.cshtml", posts);
+        }
 
         [Route("{slug}")]
         public async Task<IActionResult> SinglePublication(string slug)
