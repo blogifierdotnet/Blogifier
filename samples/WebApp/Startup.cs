a5a7645da0c7ec07568da7eaa97288c2db9708ad
rxtur <rtur.net@gmail.com>
2017-06-04 00:21:00 -0500

    set to in memory for demo sites

--- samples/WebApp/Startup.cs
+++ samples/WebApp/Startup.cs
@@ -37,8 +37,8 @@ namespace WebApp
         {
             // Add framework services.
 
-            //services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase());
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase());
+            // services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
