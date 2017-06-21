6b79ea259f3e07c7dbe2b596f88fdfa213df841d
Francis Bio <francisdotbio@users.noreply.github.com>
2017-06-21 14:35:45 +0430

    updating design of layout and dependencies

--- samples/WebApp/wwwroot/blogifier/admin/custom/js/app.js
+++ samples/WebApp/wwwroot/blogifier/admin/custom/js/app.js
@@ -1,15 +1,15 @@
 toastr.options.positionClass = 'toast-bottom-right';
 toastr.options.backgroundpositionClass = 'toast-bottom-right';
 
-$('li').removeClass('active');
-var activated = false;
-$("#side-menu li").each(function (idx, li) {
-    if (window.location.pathname.indexOf("/" + $(li).attr("class")) > 0) {
-        $(li).addClass('active');
-        activated = true;
-    }
-});
-if (activated === false) { $('.posts').addClass('active'); }
+//$('li').removeClass('active');
+//var activated = false;
+//$("#side-menu li").each(function (idx, li) {
+//    if (window.location.pathname.indexOf("/" + $(li).attr("class")) > 0) {
+//        $(li).addClass('active');
+//        activated = true;
+//    }
+//});
+//if (activated === false) { $('.posts').addClass('active'); }
 
 function profileLogOut() {
     $("#frmLogOut").submit();
