19742bac1d9e33f078413b8f2fdec6df3f1bb91f
Francis Bio <francisdotbio@users.noreply.github.com>
2017-07-08 04:47:36 +0430

    Blog Theme Design improving (in progress)

---
+++ samples/WebApp/wwwroot/blogifier/blog/custom/js/blog.js
@@ -0,0 +1,13 @@
+﻿// BLOG-SEARCH
+$(".blog-search button").click(function () {
+    $(".blog-search").addClass("active");
+    $(".blog-search input").focus();
+});
+$(".blog-search input").blur(function () {
+    $(".blog-search").removeClass("active");
+});
+
+//
+$(function () {
+    $('.post-item-text').dotdotdot();
+});
