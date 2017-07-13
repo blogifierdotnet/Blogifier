6faf28388eb85efbb8990f5872348bef3b76200b
rxtur <rtur.net@gmail.com>
2017-07-13 14:51:01 -0500

    Social buttons step two: app settings

--- src/Blogifier.Core/Services/Social/SocialService.cs
+++ src/Blogifier.Core/Services/Social/SocialService.cs
@@ -1,4 +1,5 @@
-﻿using Blogifier.Core.Data.Domain;
+﻿using Blogifier.Core.Common;
+using Blogifier.Core.Data.Domain;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,14 +9,12 @@ namespace Blogifier.Core.Services.Social
     {
         public Task<Dictionary<string, string>> GetSocialButtons(Profile profile)
         {
-            var buttons = new Dictionary<string, string>();
+            var buttons = ApplicationSettings.SocialButtons;
 
-            buttons.Add("social-google", "https://plus.google.com/u/0/");
-            buttons.Add("social-twitter", "https://twitter.com/blogifierdotnet");
-            buttons.Add("social-github", "https://github.com/blogifierdotnet");
-            buttons.Add("social-instagram", "https://www.instagram.com");
-            buttons.Add("social-facebook", "https://www.facebook.com/blogifierdotnet");
-
+            if(profile != null)
+            {
+                // override with profile customizations
+            }
             return Task.Run(()=> buttons);
         }
     }
