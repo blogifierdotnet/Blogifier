4b3915c2538372a1ef445db68866daf7f38cc613
Unknown <fstanek@gmail.com>
2017-07-17 13:21:19 -0400

    Angular Material Theme
    Initial checkin of Angular Material theme

---
+++ samples/WebApp/wwwroot/blogifier/blog/Bloodforge-AngularMaterial/js/BloodforgeAMApp.js
@@ -0,0 +1,323 @@
+﻿angular.module('bloodforgeAMApp', ['ngMaterial', 'ui.router', 'ngAnimate'])
+
+    .filter('unsafe', ['$sce', function ($sce) {
+        return $sce.trustAsHtml;
+    }])
+
+    .config(['$mdIconProvider', '$stateProvider', function ($mdIconProvider, $stateProvider) {
+
+        var shareIconPath = '/blogifier/blog/Bloodforge-AngularMaterial/images/share/';
+
+        $mdIconProvider
+            .icon('Facebook', shareIconPath + 'facebook.svg', 24)
+            .icon('Google', shareIconPath + 'google.svg', 24)
+            .icon('Twitter', shareIconPath + 'twitter.svg', 24)
+            .icon('Github', shareIconPath + 'github.svg', 24)
+            .icon('Instagram', shareIconPath + 'instagram.svg', 24)
+            .icon('LinkedIn', shareIconPath + 'linkedin.svg', 24);
+
+        $stateProvider.state('home', {
+            url: '/home/:page',
+            component: 'postsList',
+            resolve: {
+                config: ['$stateParams', function ($stateParams) {
+                    return {
+                        type: 'home',
+                        page: $stateParams.page,
+                        url: '/blogifier/api/public/posts?page={{page}}'
+                    }
+                }]
+            }
+        });
+        $stateProvider.state('post', {
+            url: '/post/:slug',
+            component: 'post',
+            resolve: {
+                config: ['$stateParams', function ($stateParams) {
+                    return {
+                        type: 'post',
+                        slug: $stateParams.slug,
+                        url: '/blogifier/api/public/posts/post/' + $stateParams.slug
+                    }
+                }]
+            }
+        });
+        $stateProvider.state('category', {
+            url: '/category/:slug/:page',
+            component: 'postsList',
+            resolve: {
+                config: ['$stateParams', function ($stateParams) {
+                    return {
+                        type: 'category',
+                        page: $stateParams.page,
+                        slug: $stateParams.slug,
+                        url: '/blogifier/api/public/posts/category/' + $stateParams.slug + '?page={{page}}'
+                    }
+                }]
+            },
+            params: {
+                page: {
+                    value: '1'
+                }
+            }
+        });
+        $stateProvider.state('author', {
+            url: '/author/:slug/:page',
+            component: 'postsList',
+            resolve: {
+                config: ['$stateParams', function ($stateParams) {
+                    return {
+                        type: 'author',
+                        page: $stateParams.page,
+                        slug: $stateParams.slug,
+                        url: '/blogifier/api/public/posts/author/' + $stateParams.slug + '?page={{page}}'
+                    }
+                }]
+            },
+            params: {
+                page: {
+                    value: '1'
+                }
+            }
+        });
+        $stateProvider.state('search', {
+            url: '/search/:page/:slug',
+            component: 'postsList',
+            resolve: {
+                config: ['$stateParams', function ($stateParams) {
+                    return {
+                        type: 'search',
+                        page: $stateParams.page,
+                        slug: $stateParams.slug,
+                        url: '/blogifier/api/public/posts/search/' + $stateParams.slug + '?page={{page}}'
+                    }
+                }]
+            },
+            params: {
+                page: {
+                    value: '1'
+                }
+            }
+        });
+    }])
+
+    .controller('bloodforgeAMController', ['$log', '$scope', '$mdSidenav', '$state', '$transitions', '$http', function ($log, $scope, $mdSidenav, $state, $transitions, $http) {
+        $transitions.onStart({}, function () {
+            $('#load-progress').show();
+        });
+        $scope.$on('loadComplete', function () {
+            $('#load-progress').hide();
+        });
+
+        $scope.blogsearch = false;
+        $scope.showSearch = function () {
+            $scope.blogsearch = true;
+            setTimeout(function () {
+                $('#term').val('').focus();
+            }, 1);
+        }
+        $('#term').keydown(function (evt) {
+            if (evt.which == 13) {
+                var query = $(this).val();
+                $state.go("search", { page: '1', slug: query });
+                $scope.blogsearch = false;
+            }
+        });
+
+        $scope.profileLogOut = function () {
+            $("#frmLogOut").submit();
+        };
+        $scope.openSidenav = function (id) {
+            $mdSidenav(id).toggle();
+        };
+
+        $scope.sidenavNavigate = function (sidenavId, targetState, pObj) {
+            $state.go(targetState, pObj);
+            $mdSidenav(sidenavId).close();
+        };
+
+        var $window = $(window);
+        $window.on('scroll', function () {
+            $scope.$apply(function () {
+                $scope.scrollY = $window.scrollTop();
+            });
+        });
+
+        $scope.scrollToTop = function () {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        };
+
+        $http({
+            method: 'GET',
+            url: '/blogifier/api/public/categories'
+        }).then(function successCallback(response) {
+            $scope.categories = response.data;
+        }, function errorCallback(response) {
+
+            });
+
+        $http({
+            method: 'GET',
+            url: '/blogifier/api/public/authors'
+        }).then(function successCallback(response) {
+            $scope.authors = response.data;
+        }, function errorCallback(response) {
+
+        });
+
+        if (!window.location.hash) {
+            $state.go('home', { page: 1 });
+        }
+    }])
+
+    .component('postsList', {
+        bindings: {
+            config: '<'
+        },
+        templateUrl: '/blogifier/blog/Bloodforge-AngularMaterial/templates/posts-list.tpl.html',
+        controller: ['$http', '$element', '$scope', function ($http, $element, $scope) {
+            this.state = 'init';
+            var ctrl = this;
+
+            $element.addClass('layout-column flex');
+
+            var loadPosts = function (url) {
+                $http({
+                    method: 'GET',
+                    url: url
+                }).then(function successCallback(response) {
+                    if (ctrl.posts) {
+                        // posts added using 'show more posts'
+                        for (var i = 0; i < response.data.posts.length; i++) {
+                            ctrl.posts.push(response.data.posts[i]);
+                        }
+                    }
+                    else {
+                        // new posts
+                        if (ctrl.config.type == 'home') {
+                            // set up featured posts
+                            ctrl.featuredPosts = response.data.posts.slice(0, 2);
+                            ctrl.posts = response.data.posts.slice(2);
+                        }
+                        else {
+                            ctrl.posts = response.data.posts;
+                        }
+
+                        switch (ctrl.config.type) {
+                            case 'category':
+                                ctrl.header = 'Posts in category: ' + $('[data-categorySlug="' + ctrl.config.slug + '"]').text();
+                                break;
+                            case 'author':
+                                if (ctrl.posts && ctrl.posts.length > 0) {
+                                    ctrl.header = 'Posts by: ' + ctrl.posts[0].authorName;
+                                }
+                                else {
+                                    ctrl.header = 'No posts found by this author.';
+                                }
+                                break;
+                            case 'search':
+                                ctrl.header = 'Search results for: ' + ctrl.config.slug;
+                                break;
+                            default:
+                                ctrl.header = undefined;
+                                break;
+                        }
+                    }                    
+                    ctrl.pager = response.data.pager;
+
+                    if (ctrl.pager && ctrl.pager.showOlder) {
+                        ctrl.moreUrl = ctrl.config.url.split('{{page}}').join(ctrl.pager.older);
+                    }
+                    else {
+                        ctrl.moreUrl = undefined;
+                    }
+
+                    ctrl.state = 'done';
+                    $scope.$emit('loadComplete');
+                }, function errorCallback(response) {
+                    ctrl.state = 'error';
+                    $scope.$emit('loadComplete');
+                });
+            }
+
+            this.$onInit = function () {
+                var url = ctrl.config.url.split('{{page}}').join(ctrl.config.page);
+                loadPosts(url);                
+            }
+
+            this.showMore = function () {
+                ctrl.state = 'more';
+                loadPosts(ctrl.moreUrl);
+            }
+        }],
+        controllerAs: 'postsListCtrl'
+    })
+
+    .component('postListItemFeatured', {
+        bindings: {
+            post: '<'
+        },
+        templateUrl: '/blogifier/blog/Bloodforge-AngularMaterial/templates/post-list-item-featured.tpl.html',
+        controller: function () {
+
+        },
+        controllerAs: 'postListItemCtrl'
+    })
+
+    .component('postListItem', {
+        bindings: {
+            post: '<'
+        },
+        templateUrl: '/blogifier/blog/Bloodforge-AngularMaterial/templates/post-list-item.tpl.html',
+        controller: function () {
+
+        },
+        controllerAs: 'postListItemCtrl'
+    })
+
+    .component('post', {
+        bindings: {
+            config: '<'
+        },
+        templateUrl: '/blogifier/blog/Bloodforge-AngularMaterial/templates/post.tpl.html',
+        controller: ['$http', '$sce', '$scope', function ($http, $sce, $scope) {
+            this.state = 'init';
+            var ctrl = this;
+
+            var loadPost = function (url) {
+                $http({
+                    method: 'GET',
+                    url: url
+                }).then(function successCallback(response) {
+                    ctrl.post = response.data;
+
+                    if (ctrl.post && ctrl.post.postCategories) {
+                        var categories = [];
+                        for (var i = 0; i < ctrl.post.postCategories.length; i++) {
+                            var catId = ctrl.post.postCategories[i].categoryId;
+                            var $catElement = $('[data-categoryId="' + catId + '"]');
+                            var catText = $catElement.text();
+                            var catSlug = $catElement.attr('data-categorySlug');
+                            categories.push({
+                                text: catText,
+                                slug: catSlug
+                            });
+                        }
+                        ctrl.categories = categories;
+                    }
+                                        
+                    ctrl.state = 'done';
+                    $scope.$emit('loadComplete');
+                }, function errorCallback(response) {
+                    ctrl.state = 'error';
+                    $scope.$emit('loadComplete');
+                });
+            }
+
+            this.$onInit = function () {
+                var url = ctrl.config.url;
+                loadPost(url);
+            }
+        }],
+        controllerAs: 'postCtrl'
+    })
