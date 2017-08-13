6cd6eed1e82a7359cc64d7a7df2c05dca5b4113e
Francis <francisdotbio@gmail.com>
2017-08-13 21:23:03 +0430

    working on the new design layout. start ...

--- samples/WebApp/wwwroot/blogifier/admin/custom/js/app.js
+++ samples/WebApp/wwwroot/blogifier/admin/custom/js/app.js
@@ -29,40 +29,6 @@ $('.dropdown-menu').on({"click": function (e) {
     }
 });
 
-$(function () {
-    $('.post-list [data-toggle="tooltip"]').tooltip({
-        offset: '-15px 0',
-        animation: false
-    });
-    $('[data-toggle="tooltip"], .btn-icon').tooltip({
-        animation: false
-    });
-});
-
-$('.admin-settings-sidebar li').removeClass('active');
-var navSidebarActivated = false;
-$(".admin-settings-sidebar li").each(function (idx, li) {
-    if (window.location.pathname.indexOf("/" + $(li).attr("class")) > 0) {
-        $(li).addClass('active');
-        navSidebarActivated = true;
-    }
-});
-if (navSidebarActivated === false) {
-    $('.admin-settings-sidebar .basic').addClass('active');
-}
-
-$('.admin-nav li').removeClass('active');
-var navAdminActivated = false;
-$(".admin-nav li").each(function (idx, li) {
-    if (window.location.pathname.indexOf("/" + $(li).attr("class")) > 0) {
-        $(li).children().addClass('active');
-        navAdminActivated = true;
-    }
-});
-if (navAdminActivated === false) {
-    $('.admin-nav .admin').children().addClass('active');
-}
-
 function profileLogOut() {
     $("#frmLogOut").submit();
 }
