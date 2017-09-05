b6b2d8e38e7c9a2c77d9a66976e55376770c0d71
rxtur <rtur.net@gmail.com>
2017-09-05 17:09:27 -0500

    Standard12 theme added

---
+++ samples/WebApp/wwwroot/blogifier/blog/standard12/js/blog.js
@@ -0,0 +1,36 @@
+﻿// cover
+$(window).on('load resize', function () {
+    if ($(window).height() <= 767 && $(window).width() <= 767) {
+        var headerHeight = $(".blog-header").outerHeight();
+        var coverHeight = $(window).height() - headerHeight;
+        $(".cover").height(coverHeight);
+    } else {
+        $(".cover").attr('style', function (i, style) {
+            return style.replace(/height[^;]+;?/g, '');
+        });
+    }
+});
+
+// logout
+function profileLogOut() {
+    $("#frmLogOut").submit();
+}
+
+// blog-search
+$(".blog-search button").click(function () {
+    $(".blog-search").addClass("active");
+    $(".blog-search input").focus();
+});
+$(".blog-search input").blur(function () {
+    $(".blog-search").removeClass("active");
+});
+
+// dotdotdot
+$(function () {
+    $('.post-item-text').dotdotdot();
+});
+
+// tooltip
+$('[data-toggle="tooltip"]').tooltip({
+    animation: false
+});
