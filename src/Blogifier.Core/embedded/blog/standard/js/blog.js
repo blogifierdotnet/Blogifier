2fb4afa1493619e1693bb88a87af10b592fb5bb1
rxtur <rtur.net@gmail.com>
2017-09-16 20:41:23 -0500

    Blog theme moved into Core

--- src/Blogifier.Core/embedded/blog/standard/js/blog.js
+++ src/Blogifier.Core/embedded/blog/standard/js/blog.js
@@ -1,36 +1,30 @@
-﻿// cover
-$(window).on('load resize', function () {
-    if ($(window).height() <= 767 && $(window).width() <= 767) {
-        var headerHeight = $(".blog-header").outerHeight();
-        var coverHeight = $(window).height() - headerHeight;
-        $(".cover").height(coverHeight);
-    } else {
-        $(".cover").attr('style', function (i, style) {
-            return style.replace(/height[^;]+;?/g, '');
-        });
-    }
-});
+﻿//// cover
+//$(window).on('load resize', function () {
+//    if ($(window).height() <= 767 && $(window).width() <= 767) {
+//        var headerHeight = $(".blog-header").outerHeight();
+//        var coverHeight = $(window).height() - headerHeight;
+//        $(".cover").height(coverHeight);
+//    } else {
+//        $(".cover").attr('style', function (i, style) {
+//            return style.replace(/height[^;]+;?/g, '');
+//        });
+//    }
+//});
 
 // logout
 function profileLogOut() {
     $("#frmLogOut").submit();
 }
 
-// blog-search
-$(".blog-search button").click(function () {
-    $(".blog-search").addClass("active");
-    $(".blog-search input").focus();
-});
-$(".blog-search input").blur(function () {
-    $(".blog-search").removeClass("active");
+// blog-header
+$(".blog-header-toggle").click(function () {
+    $(".blog-header-modal").toggleClass("active");
 });
 
-// dotdotdot
-$(function () {
-    $('.post-item-text').dotdotdot();
-});
 
 // tooltip
 $('[data-toggle="tooltip"]').tooltip({
     animation: false
 });
+
+
