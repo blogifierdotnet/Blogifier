8bada6710ac4bc1e3e2fb188ffa8f9228de9029b
rxtur <rtur.net@gmail.com>
2017-12-12 21:37:26 -0600

    Added external OneSix theme

---
+++ Packages/Themes/OneSix/PackageInfo.cs
@@ -0,0 +1,18 @@
+﻿using Blogifier.Core.Data.Models;
+
+public class PackageInfo : IPackageInfo
+{
+    public PackageListItem GetAttributes()
+    {
+        return new PackageListItem
+        {
+            Title = "OneSix",
+            Version = "1.0.0",
+            Description = "OneSix theme for Blogifier blogs.",
+            Icon = "https://avatars0.githubusercontent.com/u/19671571?v=4&amp;s=180",
+            Author = "Blogifier",
+            ProjectUrl = "https://github.com/blogifierdotnet/Blogifier",
+            Tags = "theme,onesix"
+        };
+    }
+}
