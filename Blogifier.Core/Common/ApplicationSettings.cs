5f5a2446d9697a72220f3b0cea11b71349e28c39
Michael Warnes <mawarnes@msn.com>
2018-07-22 12:54:32 +0100

    upgraded to .net 2.1 and updated dependancies additional properties added to web project

--- Blogifier.Core/Common/ApplicationSettings.cs
+++ Blogifier.Core/Common/ApplicationSettings.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using Microsoft.EntityFrameworkCore.InMemory.Infrastructure.Internal;
 using System.Reflection;
 
 namespace Blogifier.Core.Common
