ee806855e603d38ebafbb00d6abbc02aa630e6c8
rxtur <rtur.net@gmail.com>
2018-07-31 21:28:16 -0500

    Profile and password pages

--- src/App/Pages/Admin/Settings/Password.cshtml.cs
+++ src/App/Pages/Admin/Settings/Password.cshtml.cs
@@ -1,17 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Core;
+using Core.Data;
+using Core.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace App.Pages.Admin.Settings
 {
     public class PasswordModel : PageModel
     {
+        IUnitOfWork _db;
+        UserManager<AppUser> _um;
+        SignInManager<AppUser> _sm;
+
+        [BindProperty]
+        public ChangePasswordModel ChangePasswordModel { get; set; }
+
+        public PasswordModel(IUnitOfWork db, UserManager<AppUser> um, SignInManager<AppUser> sm)
+        {
+            _db = db;
+            _um = um;
+            _sm = sm;
+        }
+
         public void OnGet()
         {
+            ChangePasswordModel = new ChangePasswordModel { UserName = User.Identity.Name };
+        }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            ModelState.Clear();
+            try
+            {
+                var user = await _um.GetUserAsync(User);
+                var result = await _um.ChangePasswordAsync(user, ChangePasswordModel.OldPassword, ChangePasswordModel.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    throw new ApplicationException(result.Errors.First().Description);
+                }
+
+                await _sm.SignInAsync(user, isPersistent: false);
+
+                TempData["msg"] = Resources.Updated;
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Custom", ex.Message);
+                return Page();
+            }
+
+            return RedirectToPage("Password");
         }
     }
 }
--- src/App/Pages/Admin/Settings/Profile.cshtml.cs
+++ src/App/Pages/Admin/Settings/Profile.cshtml.cs
@@ -1,17 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Core;
+using Core.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace App.Pages.Admin.Settings
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
-        {
+        IUnitOfWork _db;
 
+        [BindProperty]
+        public Author Author { get; set; }
+
+        public ProfileModel(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Author = await _db.Authors.GetItem(u => u.AppUserName == User.Identity.Name);
+            }
+            else
+            {
+                if (!IsAdmin())
+                    RedirectToPage("~/error/403");
+
+                Author = await _db.Authors.GetItem(u => u.AppUserName == name);
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var author = _db.Authors.Single(a => a.Id == Author.Id);
+            author.DisplayName = Author.DisplayName;
+            author.Email = Author.Email;
+
+            await _db.Authors.Save(author);
+            TempData["msg"] = Resources.Updated;
+
+            if (Author.AppUserName == User.Identity.Name)
+                return RedirectToPage("Profile");
+            else
+                return Redirect($"~/admin/settings/profile?name={Author.AppUserName}");
+        }
+
+        bool IsAdmin()
+        {
+            return _db.Authors.Single(a => a.AppUserName == User.Identity.Name).IsAdmin;
         }
     }
 }
