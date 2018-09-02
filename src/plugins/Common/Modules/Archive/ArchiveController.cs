0e8f103c9b28ec8c64e2c6356a2d8eea8755839e
rxtur <rtur.net@gmail.com>
2018-09-01 23:53:26 -0500

    Plugins under own directory

---
+++ src/plugins/Common/Modules/Archive/ArchiveController.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Common.Modules.Archive
+{
+    public class ArchiveController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Logo = $"{Url.Content("~/")}{AppSettings.Logo}";
+            ViewBag.Cover = $"{Url.Content("~/")}{AppSettings.Cover}";
+            ViewBag.Title = AppSettings.Title;
+            ViewBag.Description = AppSettings.Description;
+
+            return View("~/Views/Modules/Archive/Index.cshtml");
+        }
+    }
+}
