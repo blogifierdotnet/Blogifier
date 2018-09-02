9c68b40db3fe4eb94b91edee46b5f1ffb03fb3cd
rxtur <rtur.net@gmail.com>
2018-09-02 00:06:51 -0500

    Plugin theme and widget

---
+++ src/plugins/Common/Widgets/PostList.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Common.Widgets
+{
+    [ViewComponent(Name = "PostList")]
+    public class PostList : ViewComponent
+    {
+        public PostList()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Widgets/PostList/Index.cshtml");
+        }
+    }
+}
