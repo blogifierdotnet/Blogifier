6f0e52887985b1a2d5c0eeaa3e981fcd99261ad1
rxtur <rtur.net@gmail.com>
2018-09-22 20:06:18 -0500

    Improvements to title-to-slug

--- src/Core/Extensions/StringExtensions.cs
+++ src/Core/Extensions/StringExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web;
 
 namespace Core
 {
@@ -41,18 +44,20 @@ namespace Core
         /// <returns>Slug</returns>
 		public static string ToSlug(this string title)
         {
-            if (string.IsNullOrEmpty(title))
-                return string.Empty;
-
             var str = title.ToLowerInvariant();
+            str = str.Trim('-', '_');
+
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
 
             var bytes = Encoding.GetEncoding("utf-8").GetBytes(str);
             str = Encoding.UTF8.GetString(bytes);
 
             str = Regex.Replace(str, @"\s", "-", RegexOptions.Compiled);
-            // str = Regex.Replace(str, @"^\w+$", "", RegexOptions.Compiled);
-            str = str.Trim('-', '_');
+            
             str = Regex.Replace(str, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            str = RemoveIllegalCharacters(str);
 
             return str;
         }
@@ -138,5 +143,76 @@ namespace Core
                 : string.Format("{0}B", Math.Round((double)value, decimalPlaces));
             return chosenValue;
         }
+
+        #region Helper Methods
+
+        static string RemoveIllegalCharacters(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            string[] chars = new string[] {
+                ":", "/", "?", "!", "#", "[", "]", "{", "}", "@", "*", ".", ",",
+                "\"","&", "'", "~", "$"
+            };
+
+            foreach (var ch in chars)
+            {
+                text = text.Replace(ch, string.Empty);
+            }
+
+            text = text.Replace("–", "-");
+            text = text.Replace(" ", "-");
+
+            text = RemoveUnicodePunctuation(text);
+            text = RemoveDiacritics(text);
+            text = RemoveExtraHyphen(text);
+
+            return HttpUtility.HtmlEncode(text).Replace("%", string.Empty);
+        }
+
+        static string RemoveUnicodePunctuation(string text)
+        {
+            var normalized = text.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+
+            foreach (var c in
+                normalized.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.InitialQuotePunctuation &&
+                                      CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.FinalQuotePunctuation))
+            {
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        static string RemoveDiacritics(string text)
+        {
+            var normalized = text.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+
+            foreach (var c in
+                normalized.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark))
+            {
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        static string RemoveExtraHyphen(string text)
+        {
+            if (text.Contains("--"))
+            {
+                text = text.Replace("--", "-");
+                return RemoveExtraHyphen(text);
+            }
+
+            return text;
+        }
+
+        #endregion
     }
 }
