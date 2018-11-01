3c469db48b06f1f932ef964ba570175d58989fa2
rxtur <rtur.net@gmail.com>
2018-11-01 15:37:01 -0500

    PostList widget updates

--- plugins/Common/Widgets/PostList.cs
+++ plugins/Common/Widgets/PostList.cs
@@ -1,17 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Common.Widgets
 {
     [ViewComponent(Name = "PostList")]
     public class PostList : ViewComponent
     {
-        public PostList()
+        IDataService _db;
+
+        public PostList(IDataService db)
         {
+            _db = db;
         }
 
         public IViewComponentResult Invoke()
         {
-            return View("~/Views/Widgets/PostList/Index.cshtml");
+            var model = _db.BlogPosts.All();
+
+            return View("~/Views/Widgets/PostList/Index.cshtml", model);
         }
     }
 }
