0d609f81d7f711ede9e5704f0a4d95070b801f36
rxtur <rtur.net@gmail.com>
2018-12-21 18:41:10 -0600

    Newsletter updates

--- plugins/Common/Widgets/Newsletter.cs
+++ plugins/Common/Widgets/Newsletter.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Common.Widgets
 {
@@ -15,11 +16,22 @@ namespace Common.Widgets
 
         public IViewComponentResult Invoke(string theme, string widget)
         {
-            return View("~/Views/Widgets/Newsletter/Index.cshtml");
+            var keyHeader = $"{theme}-{widget}-header";
+            var keyThankyou = $"{theme}-{widget}-thankyou";
+
+            var header = _db.CustomFields.GetCustomValue(keyHeader);
+            var thankyou = _db.CustomFields.GetCustomValue(keyThankyou);
+
+            if (string.IsNullOrEmpty(header)) { header = "<p>Like, really social. With 12 thousands followers and counting, the topics we cover and the ways readers can access our content are constantly growing.</p>"; }
+            if (string.IsNullOrEmpty(thankyou)) { thankyou = "Thank you!"; }
+
+            var model = new NewsletterModel { Header = header, ThankYou = thankyou };
+
+            return View("~/Views/Widgets/Newsletter/Index.cshtml", model);
         }
     }
 
-    [Route("widgets/api/newsletter")]
+    [Route("widgets/newsletter")]
     public class NewsletterController : Controller
     {
         IDataService _db;
@@ -33,14 +45,56 @@ namespace Common.Widgets
         [Route("subscribe")]
         public IActionResult Subscribe([FromBody]string email)
         {
-            var existing = _db.Newsletters.Single(n => n.Email == email);
-
-            if(existing == null)
+            if (!string.IsNullOrEmpty(email))
             {
-                _db.Newsletters.Add(new Core.Data.Newsletter { Email = email });
+                try
+                {
+                    var existing = _db.Newsletters.Single(n => n.Email == email);
+                    if (existing == null)
+                    {
+                        _db.Newsletters.Add(new Core.Data.Newsletter { Email = email });
+                        _db.Complete();
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    return Ok(ex.Message);
+                }
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("unsubscribe")]
+        public IActionResult Unsubscribe([FromBody]string email)
+        {
+            var existing = _db.Newsletters.Single(n => n.Email == email);
+            if (existing != null)
+            {
+                _db.Newsletters.Remove(existing);
                 _db.Complete();
             }
-            return Ok("Thank you!");
+            return Ok();
         }
+
+        [HttpPost]
+        [Route("edit")]
+        public IActionResult Edit(string txtHeader, string txtThankYou, string hdnWidget, string hdnTheme)
+        {
+            var keyHeader = $"{hdnTheme}-{hdnWidget}-header";
+            var keyThankyou = $"{hdnTheme}-{hdnWidget}-thankyou";
+
+            _db.CustomFields.SaveCustomValue(keyHeader, txtHeader);
+            _db.CustomFields.SaveCustomValue(keyThankyou, txtThankYou);
+
+            return Redirect("~/admin/settings/themes");
+        }
+    }
+
+    public class NewsletterModel
+    {
+        public string Header { get; set; }
+        public string ThankYou { get; set; }
+        public List<Core.Data.Newsletter> Emails { get; set; }
     }
 }
