348c388df7b4679ce01a9d737469f193cf457761
rxtur <rtur.net@gmail.com>
2019-02-10 16:13:20 -0600

    Added stub api controllers, removed npm configs

---
+++ src/App/Controllers/Api/AuthorsController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Data;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace App.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        IDataService _data;
+
+        public AuthorsController(IDataService data)
+        {
+            _data = data;
+        }
+
+        public async Task<ActionResult<IEnumerable<Author>>> Get(int page = 1)
+        {
+            try
+            {
+                var results = _data.Authors.All();
+
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
