0e1b47e643122a8d25354347e534b0aab11d502c
rxtur <rtur.net@gmail.com>
2020-06-13 19:51:06 -0500

    Charts and counters to components

---
+++ src/Blogifier.Models/Analytics.cs
@@ -0,0 +1,19 @@
+﻿namespace Blogifier.Models
+{
+    public class ChartItem
+    {
+        public string Label { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class ChartOption
+    {
+        public ChartSelector Id { get; set; }
+        public string Label { get; set; }
+    }
+
+    public enum ChartSelector
+    {
+        Week = 1, Month = 2, All = 3
+    }
+}
