3f7c466c58b76750d1fa795c95db0ce3ff252d4c
rxtur <rtur.net@gmail.com>
2021-02-14 16:39:47 -0600

    Localization and about page updates

--- src/Blogifier.Shared/Models/AboutModel.cs
+++ src/Blogifier.Shared/Models/AboutModel.cs
@@ -1,8 +1,9 @@
-﻿namespace Blogifier.Shared
+namespace Blogifier.Shared
 {
 	public class AboutModel
 	{
 		public string Version { get; set; }
 		public string OperatingSystem { get; set; }
+        public string DatabaseProvider { get; set; }
 	}
 }
