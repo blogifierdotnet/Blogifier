dfd0edc375ff8c22ecc56fde7e343fa1a0a6880c
rxtur <rtur.net@gmail.com>
2021-02-14 00:46:44 -0600

    User actions

--- src/Blogifier.Core/Providers/AuthorProvider.cs
+++ src/Blogifier.Core/Providers/AuthorProvider.cs
@@ -149,10 +149,18 @@ namespace Blogifier.Core.Providers
 			if (existing == null)
 				return false;
 
+            if(existing.IsAdmin && !author.IsAdmin)
+            {
+                // do not remove last admin account
+                if (_db.Authors.Where(a => a.IsAdmin).Count() == 1)
+                    return false;
+            }
+
 			existing.Email = author.Email;
 			existing.DisplayName = author.DisplayName;
 			existing.Bio = author.Bio;
 			existing.Avatar = author.Avatar;
+            existing.IsAdmin = author.IsAdmin;
 
 			return await _db.SaveChangesAsync() > 0;
 		}
