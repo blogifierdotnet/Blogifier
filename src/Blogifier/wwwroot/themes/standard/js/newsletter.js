e419fc620c3ca70ee4f0104acc4307720a01600d
Farzin <francisdotbio@gmail.com>
2021-02-20 23:56:01 +0330

    Standard Theme Footer #243

---
+++ src/Blogifier/wwwroot/themes/standard/js/newsletter.js
@@ -0,0 +1,48 @@
+
+//
+
+window.addEventListener("DOMContentLoaded", function() {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("newsletter");
+  var button = document.getElementById("newsletter-button");
+  var status = document.getElementById("newsletter-message");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Thanks!";
+  }
+
+  function error(statusX) {
+    status.innerHTML = statusX + " Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
--- src/Blogifier/wwwroot/themes/standard/js/scripts.js
+++ src/Blogifier/wwwroot/themes/standard/js/scripts.js
@@ -4,7 +4,6 @@ hljs.initHighlightingOnLoad();
 // search modal auto focus
 var myModal = document.getElementById('searchModal')
 var myInput = document.getElementById('searchFormInput')
-
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
@@ -16,10 +15,10 @@ function copyInput(elm) {
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  copyText.value= "Copied!";
+  copyText.value = "Copied!";
   copyText.classList.add("copied");
   setTimeout(function () {
-    copyText.value= copyTextStore;
+    copyText.value = copyTextStore;
     copyText.classList.remove("copied");
   }, 1000);
 }
