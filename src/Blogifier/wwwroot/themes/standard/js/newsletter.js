546c09a3fadb73fa610b1482b078e31e48023572
Farzin <francisdotbio@gmail.com>
2021-02-21 02:18:03 +0330

    Standard Theme Newsletter #243

--- src/Blogifier/wwwroot/themes/standard/js/newsletter.js
+++ src/Blogifier/wwwroot/themes/standard/js/newsletter.js
@@ -1,32 +1,45 @@
-
-//
-
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
 
   // get the form elements defined in your form HTML above
 
   var form = document.getElementById("newsletter");
-  var button = document.getElementById("newsletter-button");
-  var status = document.getElementById("newsletter-message");
+  var form_body = document.getElementById("newsletter");
+  var form_email = document.getElementById("newsletter_email");
+  var form_status = document.getElementById("newsletter_status");
 
   // Success and Error functions for after the form is submitted
 
   function success() {
     form.reset();
-    button.style = "display: none ";
-    status.innerHTML = "Thanks!";
+    form_status.classList.add("-show", "-success");
+    form_status.innerHTML = "Thanks!";
   }
 
-  function error(statusX) {
-    status.innerHTML = statusX + " Oops! There was a problem.";
+  function loading(){
+    form_status.classList.add("-show", "-loading");
+    form_status.innerHTML = "Loading...";
+  }
+
+  function error() {
+    form_status.classList.add("-show", "-error");
+    form_status.innerHTML = "Oops! There was a problem.";
   }
 
   // handle the form submission event
 
-  form.addEventListener("submit", function(ev) {
+  form.addEventListener("submit", function (ev) {
     ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data, success, error);
+    loading();
+    fetch('https://ipapi.co/json/')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var info = data.ip + '|' + data.country_name + '|' + data.region;
+        var obj = { Email: form_email.value, Ip: info };
+        var data = JSON.stringify(obj);
+        ajax(form.method, form.action, data, success, error);
+      });
   });
 });
 
@@ -36,7 +49,7 @@ function ajax(method, url, data, success, error) {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url);
   xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
     if (xhr.status === 200) {
       success(xhr.response, xhr.responseType);
