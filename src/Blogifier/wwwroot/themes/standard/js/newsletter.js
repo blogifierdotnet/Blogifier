4b7d025dd32e50d3e833d15306b70207bbf79ee1
Farzin <francisdotbio@gmail.com>
2021-03-06 20:39:06 +0330

    Newsletter #243

--- src/Blogifier/wwwroot/themes/standard/js/newsletter.js
+++ src/Blogifier/wwwroot/themes/standard/js/newsletter.js
@@ -1,24 +1,26 @@
 // get the newsletter form elements
-var form = document.getElementById("newsletter");
-var form_email = document.getElementById("newsletter_email");
-var form_status = document.getElementById("newsletter_status");
+const form = document.getElementById("newsletter");
+const form_email = document.getElementById("newsletter_email");
+const form_status = document.getElementById("newsletter_status");
+const newsletterSucessMsg = form_status.dataset.success, newsletterErrorMsg = form_status.dataset.error;
 
 // Success, Loading and Error functions
-function success() {
+function successNewsletter() {
+  form_status.innerHTML = `<div class="newsletter-msg bg-success"><div class="m-auto"> ${newsletterSucessMsg} </div></div>`;
+  setTimeout(resetNewsletter, 2000);
+}
+function loadingNewsletter() {
+  form_status.innerHTML = '<div class="newsletter-msg"><div class="m-auto spinner-border" role="status"></div></div>'
+}
+function errorNewsletter(msg) {
+  form_status.innerHTML = `<div class="newsletter-msg bg-danger"><div class="m-auto">${newsletterErrorMsg} <br> ${msg}</div></div>`;
+}
+function resetNewsletter() {
   form.reset();
-  form_status.classList.add("-show", "-success");
-  form_status.innerHTML = form_status.dataset.success;
-}
-function loading() {
-  form_status.classList.add("-show", "-loading");
-}
-function error(msg) {
-  form_status.classList.add("-show", "-error");
-  form_status.innerHTML = form_status.dataset.error;
+  form_status.innerHTML = "";
 }
 
-// subscribe function
-function subscribe(url, data) {
+function subscribeNewsletter(url, data) {
   var options = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -33,19 +35,16 @@ function subscribe(url, data) {
       }
     })
     .then(() => {
-      form.reset()
-      success();
+      successNewsletter();
     })
     .catch((err) => {
-      error();
-      console.error(err);
+      errorNewsletter(err);
     });
 }
 
-// handle the form submission event
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  loading();
+  loadingNewsletter();
   fetch('https://ipapi.co/json/')
     .then((response) => {
       if (response.status == 200) {
@@ -61,10 +60,9 @@ form.addEventListener("submit", function (e) {
         Country: loc.country_name,
         Region: loc.region
       };
-      subscribe(form.action, subscriber_data);
+      subscribeNewsletter(form.action, subscriber_data);
     })
     .catch((err) => {
-      error();
-      console.error(err);
+      errorNewsletter(err);
     });
 });
