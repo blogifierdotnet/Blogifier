45b77cf1414365a9d2f8660351a298910cbfd69d
Lin Qiao (QLIN5) <qiao.lin@volvocars.com>
2022-04-07 17:40:53 +0800

    EasyMDE issue: flash and disappear

---
+++ src/Blogifier/wwwroot/themes/standard/js/editor.js
@@ -0,0 +1,14 @@
+// var comment_text = document.getElementById('comment-area');
+// comment_text.value = "First Value";
+// function logit() {
+//     console.log("onchange detected!");
+// }
+
+// const easyMDE = new EasyMDE({
+//     element: document.getElementById('comment-area')
+// });
+// easyMDE.value('New input for **EasyMDE**');
+
+// console.log(easyMDE.element.value);
+
+const easyMDE = new EasyMDE();
