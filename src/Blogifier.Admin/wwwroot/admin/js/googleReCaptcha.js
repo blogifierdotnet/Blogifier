78792df34c7bda44074ddd07076f31823541bc57
Dino Damianoff <75574873+TeiV2@users.noreply.github.com>
2022-04-15 12:54:45 -0300

    Implemented gReCaptcha in admin login

---
+++ src/Blogifier.Admin/wwwroot/admin/js/googleReCaptcha.js
@@ -0,0 +1,24 @@
+﻿function googleReCaptcha(dotNetObject, selector, sitekeyApiRoute) {
+  let sitekeyValue = GetSiteKeyFromUrl(sitekeyApiRoute);
+  console.log(sitekeyValue)
+  return grecaptcha.render(selector, {
+    'sitekey': sitekeyValue,
+    'callback': (response) => {
+      dotNetObject.invokeMethodAsync('CallbackOnSuccess', response);
+    },
+    'expired-callback': () => {
+      dotNetObject.invokeMethodAsync('CallbackOnExpired', response);
+    }
+  });
+}
+
+function GetSiteKeyFromUrl(url) {
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", url, false);
+  xmlHttpReq.send(null);
+  return xmlHttpReq.responseText;
+}
+
+function getResponse(response) {
+  return grecaptcha.getResponse(response);
+}
---
+++ src/Blogifier/Controllers/CaptchaController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Blogifier.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Blogifier.Controllers;
+
+[ApiController]
+[Route("api/captchakey")]
+public class CaptchaController : ControllerBase
+{
+    public CaptchaController(IConfiguration configuration)
+    {
+        SiteKey = configuration.GetSection("ReCaptcha").GetValue<string>("ReCaptchaSiteKey");
+    }
+
+    private string SiteKey { get; }
+
+    [HttpGet]
+    public Task<ActionResult> GetCaptchaKeyAsJson([FromQuery(Name = "valueonly")] bool keyOnly)
+    {
+        if (keyOnly)
+            return Task.FromResult<ActionResult>(Ok(SiteKey));
+
+        return Task.FromResult<ActionResult>(
+            Ok(new CaptchaKeyModel
+            {
+                ReCaptchaSiteKey = SiteKey
+            }));
+    }
+}
