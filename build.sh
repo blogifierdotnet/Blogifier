300fdb7a514609888d99a792feb2fd0a9557e2e7
dorthl <x344527085@outlook.com>
2023-06-01 11:25:48 +0800

    docker build

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+
+# Local machine
 dotnet publish -c Release /p:RuntimeIdentifier=linux-x64 ./src/Blogifier/Blogifier.csproj --output dist
+
+# docker
+docker build -t dorthl/blogifier:latest .
+docker push dorthl/blogifier:latest
