cc5ad9accdd3c7874794c8bda3430f7d96dce365
dorthl <x344527085@outlook.com>
2023-06-02 10:44:42 +0800

    update pubush

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
-
 # Local machine
 rm -fr dist
 dotnet publish -c Release /p:RuntimeIdentifier=linux-x64 ./src/Blogifier/Blogifier.csproj --output dist
 
 # docker
+# rm -fr dist
+# dotnet publish -c Release /p:RuntimeIdentifier=linux-musl-x64 ./src/Blogifier/Blogifier.csproj --output dist
 # docker build -t dorthl/blogifier:latest .
 # docker push dorthl/blogifier:latest
