ecb8a3c834767a6dc0d77aad4a4a835bd3ed9950
dorthl <x344527085@outlook.com>
2023-06-02 11:23:53 +0800

    user editor

--- src/Blogifier.Shared/Dtos/UserInfoDto.cs
+++ src/Blogifier.Shared/Dtos/UserInfoDto.cs
@@ -4,6 +4,7 @@ public class UserInfoDto
 {
   public string Id { get; set; } = default!;
   public string Email { get; set; } = default!;
+  public string UserName { get; set; } = default!;
   public string NickName { get; set; } = default!;
   public string? Avatar { get; set; }
   public string? Bio { get; set; }
