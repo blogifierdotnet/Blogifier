c1d2194de1c8eee08cb0539686240d8e088b7b38
dorthl <x344527085@outlook.com>
2023-06-02 23:14:53 +0800

    docker publish

--- publish.sh
+++ publish.sh
@@ -1,9 +1,7 @@
 # Local machine
-rm -fr dist
-dotnet publish -c Release /p:RuntimeIdentifier=linux-x64 ./src/Blogifier/Blogifier.csproj --output dist
+# rm -fr dist
+# dotnet publish -c Release /p:RuntimeIdentifier=linux-x64 ./src/Blogifier/Blogifier.csproj --output dist
 
 # docker
-# rm -fr dist
-# dotnet publish -c Release /p:RuntimeIdentifier=linux-musl-x64 ./src/Blogifier/Blogifier.csproj --output dist
-# docker build -t dorthl/blogifier:latest .
-# docker push dorthl/blogifier:latest
+docker build -t dorthl/blogifier:latest .
+docker push dorthl/blogifier:latest
