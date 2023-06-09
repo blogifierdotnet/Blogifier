61ef46a2de0c985f4e420e82087a95e0a5cdb695
dorthl <x344527085@outlook.com>
2023-06-09 16:11:25 +0800

    fix assets map path

--- src/Blogifier.Themes.Standard/assets/rollup.config.dev.mjs
+++ src/Blogifier.Themes.Standard/assets/rollup.config.dev.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'js/index.js',
+  input: 'js/blogifier.js',
   output: {
     format: 'iife',
     file: 'dist/js/blogifier.js',
--- src/Blogifier.Themes.Standard/assets/rollup.config.mjs
+++ src/Blogifier.Themes.Standard/assets/rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'js/index.js',
+  input: 'js/blogifier.js',
   output: {
     format: 'iife',
     file: 'dist/js/blogifier.js',
