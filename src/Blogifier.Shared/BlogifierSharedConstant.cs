802bd12290cdfb36eeccad15c6de46dd1f934d66
dorthl <x344527085@outlook.com>
2023-06-19 18:20:07 +0800

    Shared Constant

--- src/Blogifier.Shared/BlogifierSharedConstant.cs
+++ src/Blogifier.Shared/BlogifierSharedConstant.cs
@@ -7,8 +7,8 @@ public static class BlogifierSharedConstant
 {
   public const string PolicyAdminName = "Administrator";
   public static readonly string PolicyAdminValue = $"{((int)UserType.Administrator)}";
-  public static string DefaultAvatar = "img/avatar.webp";
-  public static string DefaultCover = "img/cover.jpg";
-  public static string DefaultLogo = "img/logo-sm.png";
+  public static readonly string DefaultAvatar = "img/avatar.webp";
+  public static readonly string DefaultCover = "img/cover.jpg";
+  public static readonly string DefaultLogo = "img/logo-sm.png";
   public static readonly JsonSerializerOptions DefaultJsonSerializerOptionss = new(JsonSerializerDefaults.Web);
 }
