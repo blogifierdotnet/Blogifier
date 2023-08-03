e01fc30054e572a444e8cc49cbb3d3fde5f8c1e1
dorthl <x344527085@outlook.com>
2023-08-03 11:45:39 +0800

    add shared rcl

---
+++ src/Blogifier.Shared.RCL/SharedRCLConstant.cs
@@ -0,0 +1,17 @@
+namespace Blogifier.Shared.RCL;
+
+public static class SharedRCLConstant
+{
+  public const string AssemblyName = "Blogifier.Shared.RCL";
+
+  public const string AssetsSvgCategoriesPath = $"/_content/{AssemblyName}/img/categories.svg";
+  public const string AssetsSvgGithubPath = $"/_content/{AssemblyName}/img/github.svg";
+  public const string AssetsSvgTwitterPath = $"/_content/{AssemblyName}/img/twitter.svg";
+  public const string AssetsSvgFacebookPath = $"/_content/{AssemblyName}/img/facebook.svg";
+  public const string AssetsSvgYoutubePath = $"/_content/{AssemblyName}/img/youtube.svg";
+  public const string AssetsSvgInstagramPath = $"/_content/{AssemblyName}/img/instagram.svg";
+  public const string AssetsSvgSearchPath = $"/_content/{AssemblyName}/img/search.svg";
+  public const string AssetsSvgChevronDownPath = $"/_content/{AssemblyName}/img/chevron-down.svg";
+  public const string AssetsSvgBoxArrowInRightPath = $"/_content/{AssemblyName}/img/box-arrow-in-right.svg";
+  
+}
--- src/Blogifier.Themes.Standard/ThemesStandardConstant.cs
+++ src/Blogifier.Themes.Standard/ThemesStandardConstant.cs
@@ -1,6 +1,6 @@
 namespace Blogifier.Themes.Standard;
 
-internal static class Constant
+public static class ThemesStandardConstant
 {
   public const string AssemblyName = "Blogifier.Themes.Standard";
 }
